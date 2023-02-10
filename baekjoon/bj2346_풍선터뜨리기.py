56a7d6e981adfdfb3f1efabb8ef3bdaece5dd1ed
Kyuber <hkyeo98@gmail.com>
2023-02-10 21:06:32 +0900

    problem solving

--- baekjoon/bj2346_풍선터뜨리기.py
+++ baekjoon/bj2346_풍선터뜨리기.py
@@ -1,14 +1,16 @@
 from collections import deque
 
+# deque의 rotate 함수를 잘 활용하면 쉽게 해결할 수 있는 문제. 
+# collections의 전체 개념에 대해서 살펴볼 필요가 있음. 
 n = int(input())
 balloons = deque(enumerate(map(int, input().split()), start=1))
-
-print(balloons)
 s = ""
+
 for i in range(n):
     index, target = balloons.popleft()
-    print(index, target)
     s += str(index) + " "
-
-    balloons.rotate(target)
+    if target > 0:
+        balloons.rotate(-(target - 1))
+    else:
+        balloons.rotate(-target)
 print(s)
