4355ead2174422578c7dd2ae967e5792fe9279b2
Kyuber <hkyeo98@gmail.com>
2023-02-11 18:15:50 +0900

    problem solving

---
+++ baekjoon/bj1874_스택수열.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+cur_num = 1
+stack_list = []
+impossible = 0
+answer = []
+
+for i in range(n):
+    in_num = int(input())
+    while in_num >= cur_num:
+        stack_list.append(cur_num)
+        cur_num += 1
+        
+    if in_num == stack_list[-1]: 
+        answer.append(stack_list.pop())
+    else:
+        impossible = 1
+        break 
+       
+if impossible == 1:
+    print("NO")
+else:
+    for i in range(len(answer)):
+        print(answer[i])
