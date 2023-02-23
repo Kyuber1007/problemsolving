28fea4fdfa79a3b34ea6a956bf88bc7927da748c
Kyuber <hkyeo98@gmail.com>
2023-02-24 00:42:16 +0900

    problem solving

--- baekjoon/bj2346_풍선터뜨리기.py
+++ baekjoon/bj2346_풍선터뜨리기.py
@@ -1,7 +1,7 @@
 from collections import deque
 
-# deque의 rotate 함수를 잘 활용하면 쉽게 해결할 수 있는 문제. 
-# collections의 전체 개념에 대해서 살펴볼 필요가 있음. 
+# deque의 rotate 함수를 잘 활용하면 쉽게 해결할 수 있는 문제.
+# collections의 전체 개념에 대해서 살펴볼 필요가 있음.
 n = int(input())
 balloons = deque(enumerate(map(int, input().split()), start=1))
 s = ""
