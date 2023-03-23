c89f684f9c47a7c4d1018bd411851cbff3982ada
Kyuber <hkyeo98@gmail.com>
2023-03-23 14:33:46 +0900

    problem solving

--- baekjoon/bj14595_동방프로젝트.py
+++ baekjoon/bj14595_동방프로젝트.py
@@ -2,7 +2,21 @@ import sys
 input = sys.stdin.readline
 N = int(input().strip())
 M = int(input().strip())
-action = []
-for i in range(M):
-    action.append(input())
 
+room = [[i + 1] for i in range(N)]
+for i in range(M):
+    x, y = map(int, input().split())
+    index = -1
+    for j in range(len(room)):
+        if x in room[j]:
+            index = j
+            break
+    tem = index
+    new_room = []
+    while y not in room[tem]:
+        new_room += room[tem]
+        tem += 1
+    new_room += room[tem]
+    room = room[:index] + [new_room] + room[tem + 1:]
+
+print(len(room))
