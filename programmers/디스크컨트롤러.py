83b3ae1064f776ee4f3e551acb2c3fe8d15e9a1e
Kyuber <hkyeo98@gmail.com>
2023-04-15 00:35:01 +0900

    problem solving

---
+++ programmers/디스크컨트롤러.py
@@ -0,0 +1,24 @@
+from heapq import heappush, heappop
+
+def solution(jobs):
+    answer = 0
+    idx = 0
+    q = []
+    start = -1
+    cur_time, total, length = 0, 0, len(jobs)
+    
+    while idx < length:
+        count = 0
+        for i in range(length):
+            if start < jobs[i][0] <= cur_time:
+                heappush(q, [jobs[i][1], jobs[i][0]])
+        if q:
+            cur_job = heappop(q)
+            start = cur_time
+             
+            total += cur_time + cur_job[0] - cur_job[1]
+            cur_time += cur_job[0]
+            idx += 1
+        else:
+            cur_time += 1
+    return total // length
