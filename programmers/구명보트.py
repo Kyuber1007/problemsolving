2575c5b82dc3518d0854e61cfeb4f3d45232e7ce
Kyuber <hkyeo98@gmail.com>
2023-04-28 15:24:23 +0900

    problem solving

--- programmers/구명보트.py
+++ programmers/구명보트.py
@@ -1,4 +1,4 @@
-def solution(people, limit):
+def solution1(people, limit):
     answer = 0
     people.sort()
     i = 0
@@ -15,4 +15,23 @@ def solution(people, limit):
     return answer
 
 
-solution([70, 50, 80], 100)
+solution1([70, 50, 80], 100)
+
+def solution(people, limit):
+    answer = 0
+    
+    people.sort()
+    visited = [0] * len(people)
+    j = 0
+    for i in range(len(people)-1, -1, -1):
+        if visited[i] == 1:
+            continue
+        else:
+            tem = people[i]
+            
+            if tem + people[j] <= limit:
+                visited[j] = 1
+                j += 1
+            visited[i] = 1
+            answer += 1
+    return answer
