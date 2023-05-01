ff64fe503c079c30bd4f84f76e9af1062fd62bea
Kyuber <hkyeo98@gmail.com>
2023-05-01 18:58:18 +0900

    problem solving

--- programmers/짝지어제거하기.py
+++ programmers/짝지어제거하기.py
@@ -1,21 +1,13 @@
 def solution(s):
+    answer = -1
     count = 0
-    length = len(s)
-    while True:
-        for i in range(1, len(s)):
-            if s[i-1] == s[i]:
-                count = 1
-                s = s[:i-1] + s[i+1:]
-            else:
-                count = 0
-
-            if len(s) < length:
-                length = len(s)
-                break
-        if count == 0 or len(s) == 0:
-            break
-    print(1 if len(s) == 0 else 0)
-    return 1 if len(s) == 0 else 0
-
-
-solution("baabaa")
+    stack = []
+    
+    stack.append(s[0])
+    for i in range(1, len(s)):
+        if stack:
+            if stack[-1] == s[i]:
+                stack.pop()
+                continue
+        stack.append(s[i])
+    return 0 if stack else 1
